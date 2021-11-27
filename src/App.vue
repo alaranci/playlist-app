1ec9d71c1898af05cbd545238f353482d5af640b
arayra <amandavianiva@outlook.com>
2021-11-28 04:18:49 +0700

    create app.js and include it in main.js

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul>
+    <li v-for="song in playlist" :key="song.id">
+      <h2>{{ song.title }}</h2>
+      <p>{{ song.writer }}</p>
+      <span>{{ song.place }}</span>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      playlist: [
+        {
+          id: 1,
+          title: 'Real Estate',
+          writer: 'Adam',
+          place: 'Avery #1',
+        },
+        {
+          id: 2,
+          title: 'The Most Beautiful Thing',
+          writer: 'Bruno Major',
+          place: 'Avery #1',
+        },
+      ],
+    };
+  },
+};
+</script>
